ceb40d9b755fdd647f256645f723d081a29d9cfe
Ignacio M. <igmercado46@gmail.com>
2024-07-23 20:50:06 -0300

    Adds XLSX production conversion/download

--- resources/views/registrosExistentes.blade.php
+++ resources/views/registrosExistentes.blade.php
@@ -11,7 +11,7 @@
             <hr>
             @include('components.filtros')
             <button class="btn mb-3" style="background-color: #296928; color: white;" id="crearPDF">Descargar PDF</button>
-            <button class="btn mb-3" style="background-color: #7e1919; color: white;" id="crearPDF">Descargar CSV</button>
+            <button class="btn mb-3" style="background-color: #7e1919; color: white;" id="crearXLSX">Descargar XLSX</button>
         </div>
     </div>
     <div class="d-flex justify-content-center">
@@ -21,10 +21,30 @@
     {{-- link html2pdf --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.min.js"></script>
     
+    {{-- link html2xlsx --}}
+    <script src="https://unpkg.com/xlsx@0.15.1/dist/xlsx.full.min.js"></script>
+    
     <script>
+        // pasar tabla a pdf
         document.getElementById('crearPDF').addEventListener('click', () => {
             const element = document.getElementById('elementoPDF');
             html2pdf(element);
         });
+
+        // pasar tabla a xlsx
+        function tablaAExcel(type){
+            let data = document.querySelector('#elementoPDF');
+
+            let file = XLSX.utils.table_to_book(data, {sheet: "sheet1"});
+
+            XLSX.write(file, { bookType: type, bookSST: true, type: 'base64' });
+
+            XLSX.writeFile(file, 'produccion.' + type);
+        }
+
+        document.getElementById('crearXLSX').addEventListener('click', () =>  {
+            tablaAExcel('xlsx');
+        });
+
     </script>
 @endsection
