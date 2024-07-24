752db952a916c4972f084569c17b6105ef122dec
Ignacio M. <igmercado46@gmail.com>
2024-07-24 01:26:56 -0300

    Preparing the login to actually login users

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,31 +16,18 @@
                 <p class="text-white-50 mb-5">Ingrese su usuario y contraseña</p>
   
                 <div data-mdb-input-init class="form-outline form-white mb-4">
-                  <input type="email" id="typeEmailX" class="form-control form-control-lg" />
-                  <label class="form-label" for="typeEmailX">Email</label>
+                  <input type="text" id="usuario" class="form-control form-control-lg" placeholder="Usuario"/>
+                  <label class="form-label" for="usuario">Usuario</label>
                 </div>
   
                 <div data-mdb-input-init class="form-outline form-white mb-4">
-                  <input type="password" id="typePasswordX" class="form-control form-control-lg" />
-                  <label class="form-label" for="typePasswordX">Contraseña</label>
+                  <input type="password" id="contrasenia" class="form-control form-control-lg" placeholder="Contraseña"/>
+                  <label class="form-label" for="contrasenia">Contraseña</label>
                 </div>
   
                 <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">¿Olvidaste tu contraseña?</a></p>
   
-                <button data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
-  
-                <div class="d-flex justify-content-center text-center mt-4 pt-1">
-                  <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
-                  <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
-                  <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
-                </div>
-  
-              </div>
-  
-              <div>
-                <p class="mb-0">No tenés una cuenta? <a href="#!" class="text-white-50 fw-bold">Registrate</a>
-                </p>
-              </div>
+                <button data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-light btn-lg px-5 josefin" type="submit">Login</button>
   
             </div>
           </div>
